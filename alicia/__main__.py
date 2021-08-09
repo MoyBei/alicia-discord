63997e43ef2e3d9dd8be1148432df64305cda7b4
MoyBei <moybei1999@gmail.com>
2021-08-09 16:54:40 +0800

    finally alicia is online

--- alicia/__main__.py
+++ alicia/__main__.py
@@ -1,5 +1,16 @@
 # testing if it works
 # ok so i just try to entered 2nd line
 # so whatever i dk will it crash or not, but i tried
+import discord
+
+client = discord.Client()
 
 print("Have you done Seed today?")
+
+
+
+
+
+
+# put this at the end
+client.run("MjI3NzM3MTU5NDg5MTU5MTY4.V-EOwQ.boqpZAnQngkHFe8RX4vduem6Ca8")
