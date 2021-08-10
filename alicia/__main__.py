5f43360191a6510f4b984a92a3fee4fa44f49b7e
MoyBei <moybei1999@gmail.com>
2021-08-10 17:13:19 +0800

    Added basic structure. Alicia able to introduce herself when u ask her to.

--- alicia/__main__.py
+++ alicia/__main__.py
@@ -3,11 +3,26 @@
 # so whatever i dk will it crash or not, but i tried
 import discord
 
+
+print("Starting up ...")
+print("Have you done Seed today?")
 client = discord.Client()
 
-print("Have you done Seed today?")
+
+@client.event  # register async event
+async def on_ready():
+    print("The bot is ready.")
+    print(f"Logged in as {client.user}.")
 
 
+@client.event
+async def on_message(message):
+    if message.author == client.user:  # to skip when the message author is bot itself
+        return
+
+    if message.content.startswith("$hello"):
+        await message.channel.send("Hello! I'm Alicia. Currently 16 years old ><~!")
+        await message.channel.send("https://tenor.com/view/traffic-fbi-open-up-raid-gif-13450966")
 
 
 
