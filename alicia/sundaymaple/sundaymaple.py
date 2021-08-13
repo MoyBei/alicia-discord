6974875436037c3027f4729321324c6a50efb0ae
lilacse <rirakusu@protonmail.com>
2021-08-13 11:16:02 +0800

    Sunday Maple contents in external file.

--- alicia/sundaymaple/sundaymaple.py
+++ alicia/sundaymaple/sundaymaple.py
@@ -1,35 +1,16 @@
+from pathlib import Path
+from alicia_core.logging import LogType, log
 import discord
-
-current_sundaymaple = """\
-__**SUPERSTAR (v207)**__
-> __1st Sunday: 15th August 2021__
-> · Return of the Relic Hunt Daily Mission Rewards x2
-> · Rune EXP Buff effect +100%
-> · Combo Kill Orb EXP gain +300%
-> 
-> __2nd Sunday: 22nd August 2021__
-> · Return of the Relic Hunt Daily Mission Rewards x2
-> · Monster Park Clear EXP +50%
-> 
-> __3rd Sunday: 29th August 2021__
-> · Monster Collection 'Mysterious Momon' x3 issued (Look for [Sunday Maple] Here's your Sunday Maple Gift! in Star notification)
-> · 100% extra chance for new Monster Collection registration
-> 
-> __4th Sunday: 5th September 2021__
-> · 30% off Star Force Enhancement Cost
-> 
-> __5th Sunday: 12th September 2021__
-> · 50% off Spell Trace enhancement cost
-> 
-> __6th Sunday: 19th September 2021__
-> · Polo and Frito's Bounty Hunt EXP x2
-> · Defeat Flame Wolf EXP x2
-> 
-> __7th Sunday: 26th September 2021__
-> · Selective Arcane Symbol Exchange Coupon x20 and 3X EXP Coupon (15 mins) x5 issued (Look for [Sunday Maple] Here's your Sunday Maple Gift! in Star notification)
-"""
 
 
 async def sundaymaple(message):
-    message_to_send = current_sundaymaple
-    await message.channel.send(message_to_send)
+    sundaymaple_file = Path(__file__).parent / "sundaymaple.md"
+
+    messasge_to_send = ""
+    if Path(sundaymaple_file).exists():
+        messasge_to_send = Path(sundaymaple_file).read_text(encoding="UTF-8")
+    else:
+        messasge_to_send = "Sunday Maple contents not set! Please notify the bot owners."
+        log(LogType.ERROR, f"Sunday Maple contents file ({sundaymaple_file}) not found! Please create it and fill in the contents there.")
+
+    await message.channel.send(messasge_to_send)
