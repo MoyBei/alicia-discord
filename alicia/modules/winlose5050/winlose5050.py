41b8c7dab39616df773626f9f8f7414646c6770d
MoyBei <moybei1999@gmail.com>
2021-08-22 22:40:43 +0800

    Update winlose5050 to shogun pic

--- alicia/modules/winlose5050/winlose5050.py
+++ alicia/modules/winlose5050/winlose5050.py
@@ -7,14 +7,20 @@ async def winlose5050(message, client):
     if ("win" in message_lower or "lose" in message_lower) and ("5050" in message_lower or "50/50" in message_lower):
         win_lose = randint(0, 1)
         if win_lose == 0:
-            embed_message = discord.Embed(title="Oh no! You lose the 50-50!", url="https://i.imgur.com/5RLwU5H.png")
+            embed_message = discord.Embed(title="Oh no! You lose the 50-50!",
+                                          url="https://tenor.com/view/qiqi-genshin-impact-qiqi-shy-shy-loli-gif"
+                                              "-18690579")
             await message.channel.send(embed=embed_message)
-            await message.channel.send("https://i.imgur.com/5RLwU5H.png")
+            await message.channel.send("https://tenor.com/view/qiqi-genshin-impact-qiqi-shy-shy-loli-gif-18690579")
 
         elif win_lose == 1:
-            embed_message = discord.Embed(title="Oh gratz! You win the 50-50!", url="https://i.imgur.com/R3DJAGz.jpg")
+            embed_message = discord.Embed(title="Oh gratz! You win the 50-50!",
+                                          url="https://tenor.com/view/baal"
+                                              "-raiden-shogun-electro-archon"
+                                              "-genshin-impact-burst-gif-22453979")
             await message.channel.send(embed=embed_message)
-            await message.channel.send("https://i.imgur.com/R3DJAGz.jpg")
+            await message.channel.send("https://tenor.com/view/baal-raiden-shogun-electro-archon-genshin-impact-burst"
+                                       "-gif-22453979")
 
 
 
