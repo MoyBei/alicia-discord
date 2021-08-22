d0fd9d619436aeae6be4a308a833d50774331d98
MoyBei <moybei1999@gmail.com>
2021-08-22 23:13:37 +0800

    Modified hello func

--- alicia/modules/hello/hello.py
+++ alicia/modules/hello/hello.py
@@ -1,3 +1,3 @@
 async def hello(message, client):
-    await message.channel.send("Hello! I'm Alicia. Currently 16 years old ><~! (76B/56/81)"
-                "https://tenor.com/view/traffic-fbi-open-up-raid-gif-13450966")
+    await message.channel.send("Hello! I'm Alicia. Currently 16 years old ><~! (76B/56/81)")
+    await message.channel.send("https://tenor.com/view/traffic-fbi-open-up-raid-gif-13450966")
