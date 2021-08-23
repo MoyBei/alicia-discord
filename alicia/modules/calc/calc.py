5f9edd102d7048042e91cb87c6ae05e7e91fd3b1
MoyBei <moybei1999@gmail.com>
2021-08-23 20:07:46 +0800

    improve pi func

--- alicia/modules/calc/calc.py
+++ alicia/modules/calc/calc.py
@@ -1,7 +1,8 @@
 async def calc(message, client):
     calc_str = message.content.replace("$calc ", "")
+    pi_replace_str = calc_str.replace("pi", "3.14159265358979")
     try:
-        answer = eval(calc_str)
+        answer = eval(pi_replace_str)
         await message.channel.send(f"Calculate: {calc_str} = {answer}")
     except (TypeError, KeyError, NameError):
         await message.channel.send(f"`$calc [what to calculate]`")
